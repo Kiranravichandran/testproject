8315fc29563fd82ee8705cf6189b7254715cb679
Kiran <rkiran.ece@gmail.com>
2020-03-30 15:09:38 +0530

    Added changes to flask

--- test.py
+++ test.py
@@ -1,33 +1,29 @@
-from datetime import date, timedelta
-from random import randint
-import sys
-import subprocess
+from flask import Flask
+from flask import Flask, flash, redirect, render_template, request, session, abort
 import os
-
-# returns a date string for the date that is N days before STARTDATE
-def get_date_string(n, startdate):
-	d = startdate - timedelta(days=n)
-	rtn = d.strftime("%a %b %d %X %Y %z -0400")
-	return rtn
-
-# main app
-def main(argv):
-	if len(argv) < 1 or len(argv) > 2:
-		print ("Error: Bad input.")
-		sys.exit(1)
-	n = int(argv[0])
-	if len(argv) == 1:
-		startdate = date.today()
-	if len(argv) == 2:
-		startdate = date(int(argv[1][0:4]), int(argv[1][5:7]), int(argv[1][8:10]))
-	i = 0
-	while i <= n:
-		curdate = get_date_string(i, startdate)
-		num_commits = randint(1, 10)
-		for commit in range(0, num_commits):
-			subprocess.call("echo '" + curdate + str(randint(0, 1000000)) +"' > realwork.txt; sync; git add realwork.txt; GIT_AUTHOR_DATE='" + curdate + "' GIT_COMMITTER_DATE='" + curdate + "' git commit -m 'update'", shell=True)
-		i += 1
-	subprocess.call("git rm realwork.txt; git commit -m 'delete'; git push;", shell=True)
-
+ 
+app = Flask(__name__)
+ 
+@app.route('/')
+def home():
+    if not session.get('logged_in'):
+        return render_template('login.html')
+    else:
+        return "Hello Boss!  <a href='/logout'>Logout</a>"
+ 
+@app.route('/login', methods=['POST'])
+def do_admin_login():
+    if request.form['password'] == 'password' and request.form['username'] == 'admin':
+        session['logged_in'] = True
+    else:
+        flash('wrong password!')
+    return home()
+ 
+@app.route("/logout")
+def logout():
+    session['logged_in'] = False
+    return home()
+ 
 if __name__ == "__main__":
-	main(sys.argv[1:])
+    app.secret_key = os.urandom(12)
+    app.run(debug=True,host='0.0.0.0', port=4000)
